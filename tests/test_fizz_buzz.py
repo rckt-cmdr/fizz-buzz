72980fa604df1028a389810244fa8b1ef774d2a4
Jonathan Belden <jbelden@guardians.knowhere.com>
2020-08-24 18:51:01 -0600

    configure package

---
+++ tests/test_fizz_buzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# File: test_fizz-buzz.py
+# Author: Jonathan Belden
+# Description: Test module for the fizz-buzz coding challenge
+# Reference: https://edabit.com/challenge/WXqH9qvvGkmx4dMvp
+
+
+import pytest
+import fizz_buzz
+
